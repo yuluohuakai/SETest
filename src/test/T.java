b9950b68e1fef688ac1a004904f010d526b9d0d5
strong <2512046562@qq.com>
2022-07-04 23:22:59 +0800

    master fu commit

--- src/test/T.java
+++ src/test/T.java
@@ -7,6 +7,6 @@ public class T {
         System.out.println("hello git2");
         System.out.println("master test");
         System.out.println("dev test");
-        System.out.println("dev-AA re test");
+        System.out.println("dev-AA1 re test");
     }
 }
